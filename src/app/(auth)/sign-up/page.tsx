8c434d6ad04c809160233962613381d2087a142c
Strnge <2002ishavs@gmail.com>
2024-07-14 05:03:47 +0530

    Few Changes

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -84,9 +84,9 @@ const Page = () => {
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Join Mystry Message
+            Join Ishan Medicose
           </h1>
-          <p className="mb-4">Sign up to start your anonymouse adventure</p>
+          <p className="mb-4">Sign up to start your journey with our pharmacy services.</p>
         </div>
 
         <Form {...form}>
