48b4a3a753279a80c609b66ad20eb21f1c79b8b5
Strnge <2002ishavs@gmail.com>
2024-07-14 21:26:32 +0530

    few changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ export async function middleware(request: NextRequest) {
     const url = request.nextUrl;
 
     // Get the redirectUrl from query parameter or cookie
-    const redirectUrl = new URLSearchParams(request.nextUrl.search).get('redirectUrl') || '/';
+    
 
     // Redirect to dashboard if the user is already authenticated
     // and trying to access sign-in, sign-up, or home page
@@ -23,15 +23,13 @@ export async function middleware(request: NextRequest) {
             url.pathname.startsWith('/verify') ||
             url.pathname === '/')
     ) {
-        console.log(`Redirecting to: ${redirectUrl}`);
-        return NextResponse.redirect(redirectUrl);
+            return NextResponse.redirect('https://ishan-medicose.vercel.app/');
     }
 
     // Redirect to home page if the user is not authenticated
     // and trying to access dashboard
     if (!token && url.pathname.startsWith('/dashboard')) {
-        console.log(`Redirecting to: ${redirectUrl}`);
-        return NextResponse.redirect(redirectUrl);
+        return NextResponse.redirect('https://ishan-medicose.vercel.app/');
     }
 
     return NextResponse.next();
